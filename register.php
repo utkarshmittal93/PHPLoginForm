53329ff3faff0214fb8ebb5733fb29ec00a289e5
lolo <raushanjha083@gmail.com>
2018-12-23 18:25:31 +0530

    last

---
+++ register.php
@@ -0,0 +1,160 @@
+<?php
+ob_start();
+session_start();
+
+if (isset($_SESSION['user']) != "") {
+    header("Location: index.php");
+}
+include_once 'dbconnect.php';
+
+if (isset($_POST['signup'])) {
+
+
+    $fname = trim($_POST['fname']);
+    $lname = trim($_POST['lname']);
+    $uname = trim($_POST['uname']); // get posted data and remove whitespace
+    $email = trim($_POST['email']);
+    $upass = trim($_POST['pass']);
+
+    // hash password with SHA256;
+    $password = hash('sha256', $upass);
+
+    // check email exist or not
+    $stmt = $conn->prepare("SELECT email FROM users WHERE email=?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    $count = $result->num_rows;
+
+    if ($count == 0) { // if email is not found add user
+
+
+        $stmts = $conn->prepare("INSERT INTO users(firstname,lastname,username,email,password) VALUES(?, ?, ?, ?, ?)");
+        $stmts->bind_param("sssss",$fname,$lname,$uname, $email, $password);
+        $res = $stmts->execute();//get result
+        $stmts->close();
+
+        $user_id = mysqli_insert_id($conn);
+        if ($user_id > 0) {
+            $_SESSION['user'] = $user_id; // set session and redirect to index page
+            if (isset($_SESSION['user'])) {
+                print_r($_SESSION);
+                header("Location: index.php");
+                exit;
+            }
+
+        } else {
+            $errTyp = "danger";
+            $errMSG = "Something went wrong, try again";
+        }
+
+    } else {
+        $errTyp = "warning";
+        $errMSG = "Email is already used";
+    }
+
+}
+?>
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"/>
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css"/>
+</head>
+<body>
+
+<div class="container">
+
+    <div id="login-form">
+        <form method="post" autocomplete="off">
+
+            <div class="col-md-12">
+
+                <div class="form-group">
+                    <h2 class="">Register</h2>
+                </div>
+
+                <div class="form-group">
+                    <hr/>
+                </div>
+
+                <?php
+                if (isset($errMSG)) {
+
+                    ?>
+                    <div class="form-group">
+                        <div class="alert alert-<?php echo ($errTyp == "success") ? "success" : $errTyp; ?>">
+                            <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                        <input type="text" name="fname" class="form-control" placeholder="Enter Firstname" required/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                        <input type="text" name="lname" class="form-control" placeholder="Enter Lastname" required/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                        <input type="text" name="uname" class="form-control" placeholder="Enter Username" required/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                        <input type="email" name="email" class="form-control" placeholder="Enter Email" required/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                        <input type="password" name="pass" class="form-control" placeholder="Enter Password"
+                               required/>
+                    </div>
+                </div>
+
+               
+                 <div class="form-group">
+                    <button type="add" class="btn    btn-block btn-primary" name="addup" id="reg">ADD</button>
+                
+                    <button type="remove" class="btn    btn-block btn-primary" name="removeup" id="reg">REMOVE</button>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn    btn-block btn-primary" name="signup" id="reg">Register</button>
+                </div>
+
+                <div class="form-group">
+                    <hr/>
+                </div>
+
+               
+
+            </div>
+
+        </form>
+    </div>
+
+</div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/tos.js"></script>
+
+</body>
+</html>
